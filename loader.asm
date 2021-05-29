85f96943cbae1472912b8d92b40cbcd251936adf
Lawrence <macknever@gmail.com>
2021-05-29 11:43:07 -0700

    load kernel and get memory map

--- loader.asm
+++ loader.asm
@@ -16,6 +16,40 @@ start:
     test edx,(1<<26)
     jz NotSupport
 
+LoadKernel:
+    mov si,ReadPacket
+    mov word[si],0x10
+    mov word[si+2],100
+    mov word[si+4],0
+    mov word[si+6],0x1000
+    mov dword[si+8],6
+    mov dword[si+0xc],0
+    mov dl,[DriveId]
+    mov ah,0x42
+    int 0x13
+    jc  ReadError
+
+GetMemInfoStart:
+    mov eax,0xe820
+    mov edx,0x534d4150
+    mov ecx,20
+    mov edi,0x9000
+    xor ebx,ebx
+    int 0x15
+    jc NotSupport
+
+GetMemInfo:
+    add edi,20
+    mov eax,0xe820
+    mov edx,0x534d4150
+    mov ecx,20
+    int 0x15
+    jc GetMemDone
+
+    test ebx,ebx
+    jnz GetMemInfo
+
+GetMemDone:
     mov ah,0x13
     mov al,1
     mov bx,0xa
@@ -24,11 +58,13 @@ start:
     mov cx,MessageLen 
     int 0x10
 
+ReadError:
 NotSupport:
 End:
     hlt
     jmp End
 
 DriveId:    db 0
-Message:    db "long mode is supported"
+Message:    db "Get memory info done"
 MessageLen: equ $-Message
+ReadPacket: times 16 db 0
